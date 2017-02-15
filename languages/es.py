7c779ee75beb12e9f7bb1d7fde497d1fec277431
marcosajc <marcosajc@gmail.com>
2017-02-15 08:56:32 -0400

    Cambios basicos, eliminando carpeta externa

--- languages/es.py
+++ languages/es.py
@@ -145,6 +145,7 @@
 'End of impersonation': 'Fin de suplantación',
 'enter a number between %(min)g and %(max)g': 'introduzca un número entre %(min)g y %(max)g',
 'enter a value': 'introduzca un valor',
+'Enter an integer between %(min)g and %(max)g': 'Enter an integer between %(min)g and %(max)g',
 'enter an integer between %(min)g and %(max)g': 'introduzca un entero entre %(min)g y %(max)g',
 'enter date and time as %(format)s': 'introduzca fecha y hora como %(format)s',
 'Error logs for "%(app)s"': 'Bitácora de errores en "%(app)s"',
