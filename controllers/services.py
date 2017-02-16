2b1216f375c24facb268f9c92af91cc9cd1b1c5a
cserradag96 <cserradag96@gmail.com>
2017-02-16 02:07:36 -0400

    Vista de servicios añadida

---
+++ controllers/services.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+def services(): return dict()
+
+def register(): return dict()
+
+def myservices(): return dict()
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
