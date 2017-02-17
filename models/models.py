96e354f26ae6639d464b133194cd99e085347071
marcosajc <marcosajc@gmail.com>
2017-02-16 23:06:29 -0400

    Fix de migraciones para poder alterar campos

--- models/models.py
+++ models/models.py
@@ -1,10 +1,11 @@
-DB = DAL("postgres://cbvusb:1234@localhost/cbvusb")
+db = DAL("postgres://cbvusb:1234@localhost/cbvusb")
 
-DB.define_table('usuario', 
+db.define_table('usuario', 
         Field('username', type='string', length=24, required=True, notnull=True, unique=True),
-        Field('password', type='password', length=24, required=True, notnull=True))
+        Field('password', type='password', length=24, required=True, notnull=True),
+        migrate="db.usuario")
 
-DB.define_table('persona',
+db.define_table('persona',
         Field('cedula', type='integer', required=True, notnull=True, unique=True),
         Field('primer_nombre', type='string', required=True, notnull=True),
         Field('segundo_nombre', type='string'),
@@ -16,20 +17,24 @@ DB.define_table('persona',
         Field('imagen', type='text'),
         Field('email_principal', type='string', required=True, notnull=True),
         Field('email_alternativo', type='string'),
-        Field('estado_civil', type='string', required=True, notnull=True))
+        Field('estado_civil', type='string', required=True, notnull=True),
+        migrate="db.persona")
         
-DB.define_table('bombero', 
+db.define_table('bombero', 
         Field('carnet', type='integer', required=True, notnull=True, unique=True),
         Field('imagen_perfil', type='text'),
         Field('iniciales', type='string'),
         Field('tipo_sangre', type='string'),
         Field('id_persona', type='reference persona', required=True, notnull=True, unique=True), 
-        Field('id_usuario', type='reference usuario', required=True, notnull=True, unique=True))
+        Field('id_usuario', type='reference usuario', required=True, notnull=True, unique=True),
+        Field('hijos', type='integer', default=0),
+        migrate="db.bombero")
 
-DB.define_table('servicio',
+db.define_table('servicio',
     Field('CodServicio',unique = True,notnull = True),
     Field('Registra','reference bombero',notnull = True),
     Field('Borrador','boolean',default = True,notnull = True),
     Field('Aprueba','reference bombero'),
     Field('fechaCreacion','date'),
-    Field('tipo'))
+    Field('tipo'),
+    migrate="db.servicio")
