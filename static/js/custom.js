af1aaba94b6003293639f13f1c87203128d722ef
JuanTepedino <13-11021@usb.ve>
2017-03-04 18:34:09 -0400

    Fixed Instanciate Commision Prefab

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
-    $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
-    });
+$(document).ready(function(){
 
-    $("#botton").click(function(){
+  $(".clickable-row").click(function() {
+      window.location = $(this).data("href");
+  });
 
-    		// get the last DIV which ID starts with ^= "comisionPrefab"
-	var $div = $('div[id^="comisionPrefab"]:last');
+  $('#instanceComision').click(function(){
 
-	// Read the Number from that DIV's ID (i.e: 3 from "comisionPrefab3")
-	// And increment that number by 1
-	var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
+    // get the last DIV which ID starts with ^= "comisionPrefab"
+    var $div = $('div[id^="comisionPrefab"]:last');
 
-	// Clone it and assign the new ID (i.e: from num 4 to ID "comisionPrefab4")
-	var $comisionPrefab = $div.clone().prop('id', 'comisionPrefab'+num );
-    	$comisionPrefab.empty();
-    	$comisionPrefab.appendTo("#commission");
-    });
+    // Read the Number from that DIV's ID (i.e: 3 from "comisionPrefab3")
+    // And increment that number by 1
+    var num = parseInt( $div.prop("id").match(/\d+/g), 10 ) +1;
 
+    // Clone it and assign the new ID (i.e: from num 4 to ID "comisionPrefab4")
+    var $comisionPrefab = $div.clone().prop('id', 'comisionPrefab'+num );
+
+    $comisionPrefab.appendTo("#instances");
+
+  });
 });
