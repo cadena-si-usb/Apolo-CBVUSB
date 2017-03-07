b9edbc1c780db78e8dd4c9458ffa759319a57570
lalezkadu <lalezkaduque@gmail.com>
2017-03-07 17:27:39 -0400

    Algunos cambios de hoy, no me dio chance probarlos, esperen.

--- controllers/default.py
+++ controllers/default.py
@@ -157,70 +157,6 @@ def perfilmodth():
 	return dict(form=form)
 
 def registrousrth():
-
-	"""
-	form = SQLFORM.factory(
-		db.usuario,
-		db.persona,
-		db.bombero)
-
-	
-	username = request.vars.getlist("username")
-	password = request.vars.getlist("password")
-	cedula = request.vars.getlist("cedula")
-	primer_nombre = request.vars.getlist("primer_nombre")
-	segundo_nombre = request.vars.getlist("segundo_nombre")
-	primer_apellido = request.vars.getlist("primer_apellido")
-	segundo_apellido = request.vars.getlist("segundo_apellido")
-	fecha_nacimiento = request.vars.getlist("fecha_nacimiento")
-	lugar_nacimiento = request.vars.getlist("lugar_nacimiento")
-	genero = request.vars.getlist("genero")
-	email_principal = request.vars.getlist("email_principal")
-	estado_civil = request.vars.getlist("estado_civil")
-	carnet = request.vars.getlist("carnet")
-	tipo_sangre = request.vars.getlist("tipo_sangre")
-
-	if (username == [] or password == [] or cedula == [] or 
-		primer_nombre == [] or segundo_nombre == [] or primer_apellido == [] or 
-		segundo_apellido == [] or fecha_nacimiento == [] or 
-		lugar_nacimiento == [] or genero == [] or email_principal == [] or 
-		estado_civil == [] or carnet == [] or tipo_sangre == []):
-		pass
-	elif (username[0] == "" or password[0] == "" or cedula[0] == "" or 
-		primer_nombre[0] == "" or segundo_nombre[0] == "" or 
-		primer_apellido[0] == "" or segundo_apellido[0] == "" or 
-		fecha_nacimiento[0] == "" or lugar_nacimiento[0] == "" or 
-		genero[0] == "" or email_principal[0] == "" or estado_civil[0] == "" or 
-		carnet[0] == "" or tipo_sangre[0] == ""):
-		pass
-	else:
-		db.usuario.insert(
-							username=username[0], 
-							password=password[0])
-		db.persona.insert(
-							cedula=int(cedula[0]), 
-							primer_nombre=primer_nombre[0], 
-							segundo_nombre=segundo_nombre[0], 
-							primer_apellido=primer_apellido[0], 
-							segundo_apellido=segundo_apellido[0], 
-							fecha_nacimiento=fecha_nacimiento[0], 
-							lugar_nacimiento=lugar_nacimiento[0], 
-							genero=genero[0], 
-							email_principal=email_principal[0], 
-							estado_civil=estado_civil[0])
-		usuario = db(db.usuario.username==username[0]).select(db.usuario.id)[0]
-		id_usuario = usuario.id
-		persona = db(db.persona.cedula==cedula[0]).select(db.persona.id)[0]
-		id_persona = persona.id
-		db.bombero.insert(
-							carnet=int(carnet[0]), 
-							tipo_sangre=tipo_sangre[0], 
-							id_persona=id_persona, 
-							id_usuario=id_usuario)
-
-	return dict()
-	"""
-
 	formUsuario = SQLFORM(db.usuario)
 	formPersona = SQLFORM(db.persona)
 	formBombero = SQLFORM(db.bombero)
@@ -340,7 +276,8 @@ def buscarth():
 													(db.bombero.id_persona == db.persona.id) & 
 													(db.persona.id == db.usuario.id) & 
 													(db.bombero.id == db.usuario.id)),
-								distinct=db.persona.id)
+								distinct=db.persona.id,
+								orderby=~db.bombero.carnet) # =~ se refiere al orden descendente
 
 			if len(tabla) == 0:
 				error = True
@@ -350,7 +287,8 @@ def buscarth():
 		tabla = db(db.persona).select(join=db.bombero.on(db.bombero.id_persona == db.persona.id))
 
 	if error:
-		response.flash = 'No hay registro para esta búsqueda.'
+		if busqueda[0] != None:
+			response.flash = 'No hay registro para esta búsqueda.'
 		tabla = db(db.persona).select(join=db.bombero.on(db.bombero.id_persona == db.persona.id))
 
 	return dict(tabla=tabla)
