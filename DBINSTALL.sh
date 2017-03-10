7ce22687720da84ad5d75585b07638932bf50f42
root <root@bomberos.dex.usb.ve>
2017-03-10 08:52:20 -0400

    DBINSTALL

--- DBINSTALL.sh
+++ DBINSTALL.sh
@@ -2,10 +2,9 @@
 sudo apt-get -y install python-psycopg2 postgresql
 sudo /etc/init.d/postgresql restart
 sudo rm ~/.psql_history 2> /dev/null
-sudo rm -f databases/*
+sudo rm -v -f databases/*
 sudo -u postgres dropdb cbvusb
 sudo -u postgres dropuser cbvusb
 sudo -u postgres createuser -PE -s cbvusb
 sudo -u postgres createdb -O cbvusb -E UTF8 cbvusb
 rm ~/.psql_history 2> /dev/null
-sudo /etc/init.d/apache2 stop
