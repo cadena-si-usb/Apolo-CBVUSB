8ddd854559872adb76db1f8f21ead8fe95d7603f
root <root@bomberos.dex.usb.ve>
2017-03-09 21:28:43 -0400

    Better DBINSTALL and server update

--- models/DBINSTALL.sh
+++ models/DBINSTALL.sh
@@ -1,15 +1,11 @@
 #! /bin/bash
-sudo apt-get -y install python-psycopg2
-#read -s -p "Por favor ingrese su contraseña para la base de datos de postgres: "  password
-#echo "ALTER USER postgres WITH PASSWORD '$password';" > first.sql
-#psql postgres -f first.sql
+sudo apt-get -y install python-psycopg2 postgresql
+sudo /etc/init.d/postgresql restart
 rm ~/.psql_history 2> /dev/null
 rm -f ../databases/*
 sudo -u postgres dropdb cbvusb
+sudo -u postgres dropuser cbvusb
 sudo -u postgres createuser -PE -s cbvusb
 sudo -u postgres createdb -O cbvusb -E UTF8 cbvusb
-#read -s -p "Por favor ingrese su contraseña para la base de datos del sistema Apolo: " password
-#echo "ALTER USER cvusb WITH PASSWORD '$password';" > first.sql
-#psql cbvusb -f first.sql
 rm ~/.psql_history 2> /dev/null
-#rm first.sql
+sudo /etc/init.d/apache2 stop
