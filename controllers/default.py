0238ac8cbb5aaa4e19fd7bfa59a750d072a1096f
marcosajc <marcosajc@gmail.com>
2017-03-16 20:09:19 -0400

    Cambios para poder confirmar usuarios y negarlos

--- controllers/default.py
+++ controllers/default.py
@@ -20,6 +20,19 @@ def usernotconfirmed():	# HAY QUE VER SI EXISTE EL USUARIO EN CONFIRMACIÓN PARA
 	T.force('es')
 	tipo = ""
 
+	default_cedula = ""
+	default_genero = ""
+	default_carnet = ""
+
+	bombero = db(db.bombero.id_usuario==auth.user.id).select()
+
+	if len(bombero) != 0:
+		persona = db(db.persona.id==bombero.first().id_persona).select().first()
+
+		default_cedula = persona.cedula
+		default_genero = persona.genero
+		default_carnet = bombero.first().carnet
+
 	formConfirmar = SQLFORM.factory(
 		Field('password', 
 			type='password', 
@@ -31,13 +44,14 @@ def usernotconfirmed():	# HAY QUE VER SI EXISTE EL USUARIO EN CONFIRMACIÓN PARA
 		Field('password_again',
 			type='password', 
 			readable=False, 
-			length=512, 
+			length=512,
 			requires=db.usuario.password.requires,
 			label='Reingrese la clave'
 			),
 		Field('cedula', 
 			type='integer',
-			length=512, 
+			length=512,
+			default=default_cedula,
 			requires=db.persona.cedula.requires,
 			label='Cédula'
 			),
@@ -58,6 +72,7 @@ def usernotconfirmed():	# HAY QUE VER SI EXISTE EL USUARIO EN CONFIRMACIÓN PARA
 		Field('genero', 
 			type='string', 
 			notnull=True,
+			default=default_genero,
 			requires=db.persona.genero.requires,
 			label='Género'
 			),
@@ -71,29 +86,35 @@ def usernotconfirmed():	# HAY QUE VER SI EXISTE EL USUARIO EN CONFIRMACIÓN PARA
 		Field('carnet', 
 			type='integer', 
 			unique=True, 
+			default=default_carnet,
 			requires=db.bombero.carnet.requires,
 			label='Carnet'
 			)
-		) 
+		)
 
-	if formConfirmar.process(session=None, formname='confirmar', keepvalues=True).accepted and formConfirmar.vars.password==formConfirmar.vars.password_again:
-		
-		db(db.usuario.id==auth.user.id).update( first_name=formConfirmar.vars.primer_nombre, last_name=formConfirmar.vars.primer_apellido, **db.usuario._filter_fields(formConfirmar.vars))
-		id_persona = db.persona.insert( **db.persona._filter_fields(formConfirmar.vars))
-		db.bombero.insert( id_usuario=auth.user.id, id_persona=id_persona, **db.bombero._filter_fields(formConfirmar.vars))
-		
-		tipo="success"
-		response.flash = 'La solicitud de confirmación fue enviada exitosamente, le será notificado el resultado'
+	bombero = db(db.bombero.id_usuario==auth.user.id).select()
+	print type(formConfirmar)
 
-	elif formConfirmar.process(session=None, formname='confirmar', keepvalues=True).accepted:
-		tipo="danger"
-		response.flash = 'Las contraseñas ingresadas no son iguales'
+	if formConfirmar.process(session=None, formname='confirmar', keepvalues=True).accepted:
+		if formConfirmar.vars.password==formConfirmar.vars.password_again:
+			if len(bombero) == 0:
+				db(db.usuario.id==auth.user.id).update( first_name=formConfirmar.vars.primer_nombre, last_name=formConfirmar.vars.primer_apellido, **db.usuario._filter_fields(formConfirmar.vars))
+				id_persona = db.persona.insert( **db.persona._filter_fields(formConfirmar.vars))
+				db.bombero.insert( id_usuario=auth.user.id, id_persona=id_persona, **db.bombero._filter_fields(formConfirmar.vars))
+			
+			else:
+				db(db.usuario.id==auth.user.id).update( first_name=formConfirmar.vars.primer_nombre, last_name=formConfirmar.vars.primer_apellido, **db.usuario._filter_fields(formConfirmar.vars))
+				db(db.bombero.id_usuario==auth.user.id).update( **db.persona._filter_fields(formConfirmar.vars))
+				db.bombero.insert( id_usuario=auth.user.id, id_persona=id_persona, **db.bombero._filter_fields(formConfirmar.vars))
+
+			tipo="success"
+			response.flash = 'La solicitud de confirmación fue enviada exitosamente, le será notificado el resultado'
+		else:
+			tipo="danger"
+			response.flash = 'Las contraseñas ingresadas no son iguales'
 
 	elif formConfirmar.errors:
-
 		tipo="danger"
-		print formConfirmar.vars.password
-		print formConfirmar.vars.password_again
 		response.flash = 'Todos los campos deben ser llenados'
 
 	return dict(form=formConfirmar, tipo=tipo)
@@ -107,24 +128,94 @@ def confirmar():
 
 	if request.args:
 		opcion = request.args[0]
-		id_bombero = request.args[1]
+		id_bombero = int(request.args[1])
 		bombero = db(db.bombero.id==id_bombero).select().first()
 
 		if opcion == 'editar':
-			redirect(URL("default","editar"))
+			redirect(URL("default","editarnoconfirmado", args=[id_bombero]))
 
 		if opcion == 'eliminar':
 			db(db.persona.id == bombero.id_persona).delete()
-			bombero.delete()
+			db(db.bombero.id==id_bombero).delete()
 
 		if opcion == 'confirmar':
 			db(db.usuario.id==bombero.id_usuario).update(confirmed=True)
 
-	else:
-		tabla = db(db.usuario).select(join=db.bombero.on(db.bombero.id_persona == db.persona.id),
-										distinct=db.bombero.carnet,
-										orderby=~db.bombero.carnet)
+	tabla = db((db.bombero.id_persona==db.persona.id) & (db.usuario.id==db.bombero.id_usuario) & (db.usuario.confirmed == False)).select( distinct=db.bombero.carnet, orderby=~db.bombero.carnet)
+
+	print tabla
+
 	return dict(tabla=tabla)
+
+def editarnoconfirmado():
+	T.force('es')
+	tipo = ""
+	
+	id_bombero = int(request.args[0])
+	bombero = db(db.bombero.id==id_bombero).select().first()
+
+	persona = db(db.persona.id==bombero.id_persona).select().first()
+	usuario = db(db.usuario.id==bombero.id_usuario).select().first()
+
+	formEditar = SQLFORM.factory(
+		Field('carnet', 
+			type='integer', 
+			unique=True,
+			default=bombero.carnet,
+			requires=db.bombero.carnet.requires,
+			label='Carnet'
+			),
+		Field('cedula', 
+			type='integer',
+			length=512, 
+			default=persona.cedula,
+			requires=db.persona.cedula.requires,
+			label='Cédula'
+			),
+		Field('primer_nombre', 
+			type='string', 
+			notnull=True, 
+			default=usuario.first_name, 
+			requires=db.persona.primer_nombre.requires,
+			label='Primer nombre'
+			),
+		Field('primer_apellido', 
+			type='string', 
+			notnull=True, 
+			default=usuario.last_name, 
+			requires=db.persona.primer_apellido.requires,
+			label='Primer apellido'
+			),
+		Field('genero', 
+			type='string', 
+			notnull=True,
+			default=persona.genero,
+			requires=db.persona.genero.requires,
+			label='Género'
+			),
+		Field('email_principal', 
+			type='string', 
+			notnull=True,
+			default=usuario.email, 
+			requires=db.persona.email_principal.requires,
+			label='Email principal'
+			)
+		)
+		
+	if formEditar.process(session=None, formname='editar', keepvalues=True).accepted:
+		
+		db.usuario.update( first_name=formEditar.vars.primer_nombre, last_name=formEditar.vars.primer_apellido, **db.usuario._filter_fields(formEditar.vars))
+		db.persona.update( **db.persona._filter_fields(formEditar.vars))
+		db.bombero.update( **db.bombero._filter_fields(formEditar.vars))
+		
+		tipo="success"
+		response.flash = 'La solicitud de confirmación fue enviada exitosamente, le será notificado el resultado'
+
+	elif formEditar.errors:
+		tipo="danger"
+		response.flash = 'Todos los campos deben ser llenados'
+
+	return dict(form=formEditar, tipo=tipo)
 
 def index():
 	"""
