08af36e3896ef31bdaf64d8020a1ccaa48e827b7
Lalezka Duque <lalezkaduque@gmail.com>
2017-03-18 22:44:29 -0400

    jquery para carnet segun rango

--- static/js/th/th.js
+++ static/js/th/th.js
@@ -200,15 +200,13 @@ $(document).ready(function() {
     // Función para los botones para añadir telefonos
     $("body").on("click","button.agregartlf", function() {
     var num1 = parseInt(this.id.match(/\d+/g), 10 );   // Obtener el número del usuario al que corresponde el telefono
-    var tlfs = "#tlf" + num1;                         // Generar el identificador al contenedor de la unidad correspondiente
 
     tlfsCont[num1-1]++;                                // Aumentar el contador de telefonos para el usuario
     var num2 = tlfsCont[num1-1];                           // Variable auxiliar para la sustitucion en el html de abajo
 
     // Inserción del html
-    $(tlfs).append(
-        '<div class="form-horizontal" enctype="multipart/form-data" method="POST" id="tlf'+num2+'">\
-            <div class="form-group">\
+    $("#tlf").append(
+            '<div class="form-group" id="tlf'+num2+'">\
                 <div class="col-xs-12 col-sm-9 col-sm-offset-3">\
                     <div class="col-xs-12 col-sm-4" style="padding-top: 2%;">\
                         <select name="tipotlf'+num1+'-'+num2+'" class="form-control" data-validation="required">\
@@ -222,8 +220,7 @@ $(document).ready(function() {
                         <input type="tel" class="form-control" id="tlf'+num1+'-'+num2+'" name="tlf'+num1+'-'+num2+'" data-validation="length" data-validation-length="12-20" data-validation="number" data-validation-allowing="-+()" placeholder="Teléfono...">\
                     </div>\
                 </div>\
-            </div>\
-         </div>'
+            </div>'
         );
     });
 
@@ -232,15 +229,13 @@ $(document).ready(function() {
     // Función para los botones para añadir direcciones
     $("body").on("click","button.agregardireccion", function() {
     var num1 = parseInt(this.id.match(/\d+/g), 10 );   // Obtener el número del usuario al que corresponde el telefono
-    var dirs = "#dir" + num1;                         // Generar el identificador al contenedor de la unidad correspondiente
 
     dirsCont[num1-1]++;                                // Aumentar el contador de telefonos para el usuario
     var num2 = dirsCont[num1-1];                         // Variable auxiliar para la sustitucion en el html de abajo
 
     // Inserción del html
-    $(dirs).append(
-        '<div class="form-horizontal" enctype="multipart/form-data" method="POST" id="dir'+num2+'">\
-            <div class="form-group">\
+    $("#direccion").append(
+            '<div class="form-group" id="dir'+num2+'">\
                 <div class="col-xs-12 col-sm-9 col-sm-offset-3">\
                     <div class="col-xs-12 col-sm-4" style="padding-top: 2%;">\
                         <select id="direccion_tipo'+num1+'-'+num2+'" name="direccion_tipo'+num1+'-'+num2+'" class="form-control"  data-validation="string" data-validation="required">\
@@ -257,8 +252,24 @@ $(document).ready(function() {
                 <div class="col-xs-12 col-sm-9 col-sm-offset-3" style="padding-top: 2%;">\
                     <input type="dir_d" class="form-control" id="direccion_descripcion'+num1+'-'+num2+'" name="direccion_descripcion'+num1+'-'+num2+'" data-validation="string" data-validation="required" placeholder="Descripción...">\
                 </div>\
-            </div>\
-         </div>'
+            </div>'
         );
+    });
+
+    // Funcion para ingresar carnet solo cuando tu cargo no es estudiante
+    $("select[name=no_table_cargo]").change(function(){
+        var cargo = $('select[name=no_table_cargo]').val();
+        if (cargo != "Estudiante") {
+            $("#agregarcarnet").append(
+                '<div id="no_table_carnet__row" class="form-group">\
+                    <label if="no_table_carnet__label" class="control-label col-sm-3" for="no_table_carnet">Carnet *</label>\
+                    <div class="col-sm-9">\
+                        <input id="no_table_carnet" class="form-control string" type="text" name="carnet">\
+                    </div>\
+                </div>');
+        }
+        else {
+            $("#no_table_carnet__row").remove();
+        }
     });
 });
