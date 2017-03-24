199d8ff7d795277ba32cf69f7c02fc26d3f97d57
Lalezka Duque <lalezkaduque@gmail.com>
2017-03-23 23:39:00 -0400

    actualizacion js

--- static/js/thValidatorModule.js
+++ static/js/thValidatorModule.js
@@ -3,7 +3,7 @@ $.formUtils.addValidator({
   validatorFunction : function(value, $el, config, language, $form) {
     var text = $('#tlf_msg').val();
     var expreg = /^\d+$/;
-    return ((text.length == 11) && (text.match(expreg)));
+    return ((text.length == 11 || text.length == 0) && (expreg.test(text)));
   },
   errorMessage : 'El número telefónico debe tener 11 dígitos. Además no debe contener letras o símbolos.',
   errorMessageKey: 'numeroTelefono'
@@ -13,8 +13,8 @@ $.formUtils.addValidator({
   name : 'nombreContacto',
   validatorFunction : function(value, $el, config, language, $form) {
     var text = $('#persona_contacto_nombre').val();
-    var expreg = /^[0-9a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑ-\s]+$/;;
-    return (text.match(expreg));
+    var expreg = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]([a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+[\s-]?[a-zA-ZñÑáéíóúÁÉÍÓÚ\s][a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+)*$/;;
+    return (expreg.test(text));
   },
   errorMessage : 'Debe ser no vacío y contener sólo letras, guiones o espacios.',
   errorMessageKey: 'nombreContacto'
