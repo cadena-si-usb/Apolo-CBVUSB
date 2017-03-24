34062301bba8f51b561d009584e15c6f279ff9b7
Lalezka Duque <lalezkaduque@gmail.com>
2017-03-23 23:31:47 -0400

    validacion con bugs

--- static/js/thValidatorModule.js
+++ static/js/thValidatorModule.js
@@ -3,8 +3,19 @@ $.formUtils.addValidator({
   validatorFunction : function(value, $el, config, language, $form) {
     var text = $('#tlf_msg').val();
     var expreg = /^\d+$/;
-    return ((text.length == 11) && (expreg.text(text)));
+    return ((text.length == 11) && (text.match(expreg)));
   },
   errorMessage : 'El número telefónico debe tener 11 dígitos. Además no debe contener letras o símbolos.',
   errorMessageKey: 'numeroTelefono'
+});
+
+$.formUtils.addValidator({
+  name : 'nombreContacto',
+  validatorFunction : function(value, $el, config, language, $form) {
+    var text = $('#persona_contacto_nombre').val();
+    var expreg = /^[0-9a-zA-ZáéíóúàèìòùÀÈÌÒÙÁÉÍÓÚñÑ-\s]+$/;;
+    return (text.match(expreg));
+  },
+  errorMessage : 'Debe ser no vacío y contener sólo letras, guiones o espacios.',
+  errorMessageKey: 'nombreContacto'
 });
