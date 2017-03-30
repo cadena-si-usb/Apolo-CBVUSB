4273df96cc81313994f3579360a5511c0b9ef8a1
JuanTepedino <13-11021@usb.ve>
2017-03-30 05:32:52 -0400

    Updating PDF exporting views

--- static/js/services/pdfEstadistica.js
+++ static/js/services/pdfEstadistica.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
+  var divWidth = $('#export').width();
+  var divHeight = $('#export').height();
   html2canvas(document.body, {
     onrendered: function(canvas){
       var img = canvas.toDataURL("image/png");
       var doc = new jsPDF("p", "pt", "letter");
-      doc.addImage(img, 'JPEG', -80, 50, 795, 400);
-      doc.save('Estadisticas.pdf');
+
+      divWidth = divWidth * 0.90;
+      divHeight = divHeight * 0.50;
+
+      //doc.addImage(img, 'JPEG', -90, 50, divWidth, divHeight);
+      doc.addImage(img, 'JPEG', -90, 50, 795, 380);
+      var currentTime = new Date();
+      doc.save('Estadistica_' + currentTime + '.pdf');
     },
   });
 });
--- static/js/services/pdfServicio.js
+++ static/js/services/pdfServicio.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+  var divWidth = $('#export').width();
+  var divHeight = $('#export').height();
   html2canvas(document.body, {
     onrendered: function(canvas){
       var img = canvas.toDataURL("image/png");
       var doc = new jsPDF("p", "pt", "letter");
-      doc.addImage(img, 'JPEG', -80, 50, 795, 400);
-      doc.save('Servicio.pdf');
+
+      divWidth = divWidth * 0.90;
+      divHeight = divHeight * 0.57;
+
+      doc.addImage(img, 'JPEG', -85, 50, divWidth, divHeight);
+      var currentTime = new Date();
+      doc.save('Servicio_' + currentTime + '.pdf');
     },
   });
 });
